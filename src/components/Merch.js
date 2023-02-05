f8573399c035e143d28fcd6a92e4b708a2927120
Kaushal Barhate <barhate.kaushal@gmail.com>
2023-02-05 10:49:26 +0530

    first commit

---
+++ src/components/Merch.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Merch.css";
+export default function Merch() {
+  return (
+    <div>
+      <img src="./assets/merch.png" alt="Merch" className="" />
+
+      <h1>Merch</h1>
+    </div>
+  );
+}
